f53a731af81c1eaf46f7d25f534194b053c9095b
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-08-13 23:47:37 +0530

    route not found

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import routeNotFound from './app/middlewares/notFound';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// route Not found
+app.use(routeNotFound);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
